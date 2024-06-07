b2b541f564f16a95bc8a04ced1e2d6c2232a3e23
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-06-07 16:26:32 +0530

    new commit

---
+++ Controller/LikeController.js
@@ -0,0 +1,36 @@
+const LikeModel=require('../Models/LikeModel')
+const PostModel=require('../Models/PostModel')
+
+exports.likeController=async (req,res)=>{
+    try {
+        const {post,user}=req.body;
+
+        const likedPost=await LikeModel.create({
+            post,user
+        })
+//post represents the id of post made
+        const updatedPost=PostModel.findByIdAndUpdate(post,{$push:{likes:likedPost._id}},{new:true})
+        .populate("likes")
+        .exec();
+
+        res.status(200).json({
+            success:true,
+            liked:updatedPost
+        })
+
+
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            liked:"Post Cannot be liked as id not found"
+        })
+    }
+}
+
+exports.UnlikeController=async(req,res)=>{
+    try {
+        const 
+    } catch (error) {
+        
+    }
+}
--- Routes/blogroute.js
+++ Routes/blogroute.js
@@ -8,6 +8,7 @@ const router=express.Router();
 const { commentController } = require('../Controller/CommentController');
 
 const { CreatePost, fetchPost } = require('../Controller/PostController');
+const { likeController } = require('../Controller/LikeController');
 
 
 
@@ -18,6 +19,8 @@ router.post("/comment/create",commentController)
 router.post('/posts/create',CreatePost)
 
 router.get("/posts",fetchPost)
+
+router.post('/likes/like',likeController)
 
  // Export the router
  module.exports=router;
