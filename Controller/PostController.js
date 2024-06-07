86bc3eb60e62ec0f22151f1108322101b37333c3
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-06-07 15:53:07 +0530

    new commit

--- Controller/PostController.js
+++ Controller/PostController.js
@@ -3,12 +3,24 @@ const PostModel=require('../Models/PostModel')
 exports.CreatePost=async(req,res)=>{
     try {
         const {title,body}=req.body;
+
         const post=new PostModel({
             title,body
         })
+
         const savedPost=await post.save()
-    } catch (error) {
-        
+
+        res.status(200).json({
+            success:true,
+            post:savedPost
+        })
+
+    } 
+    catch (error) {
+        res.status(500).json({
+            success:false,
+            post:"No Post created"
+        })        
     }
 }
    
--- Routes/blogroute.js
+++ Routes/blogroute.js
@@ -5,14 +5,17 @@ const router=express.Router();
 
  //Import Controller
 
- const { PostController } = require('../Controller/PostController');
 const { commentController } = require('../Controller/CommentController');
+
+const { CreatePost } = require('../Controller/PostController');
 
 
 
  //Mapping of controllerto the route
 
 router.post("/comment/create",commentController)
+
+router.post('/posts/create',CreatePost)
 
  // Export the router
  module.exports=router;
