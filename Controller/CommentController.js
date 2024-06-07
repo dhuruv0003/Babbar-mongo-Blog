af438246801c79b03e65f99740c8f32ce760ffcf
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-06-07 15:10:46 +0530

    new comment

--- Controller/CommentController.js
+++ Controller/CommentController.js
@@ -1,6 +1,48 @@
-const CommentModel=require('../Models/CommentModel')
-const PostModel=require('../Models/PostModel')
+const Comment=require('../Models/CommentModel');
+const PostModel = require('../Models/PostModel');
+
 
 exports.commentController=async(req,res)=>{
-    
+    try {
+        // fetch comment data from request body 
+        
+        const {post, user,body}=req.body;
+        //create new comment object
+
+   //Method 1  
+        //const comment=await CommentModel.create({
+        //     post,user,body
+        // })
+
+        // another method to create object in db 
+    //Method 2
+        const comment=new Comment({
+            post,user,body
+        })
+        //Save new comment into database
+
+        const savecomment=await comment.save()
+
+// After making comment we also want to save the comment in the comment array of post model
+//  find the post by ID, and add the new comment to id in comment array of Post 
+
+
+// Here Post is name of model and post represents the id of post 
+
+// $push is used to push data into an array and $pull is used to delete data from array 
+        const updatedPost=await PostModel.findByIdAndUpdate(post,{$push:{comments:savecomment._id}},{new:true})
+//  new:true is used to get the updated document after updattion 
+
+// push the id of savecomment into comments array of PostModel
+
+        res.status(200).json({
+            success:true,
+            data:comment,
+
+        })
+
+
+    } catch (error) {
+        
+    }
 }
--- Models/PostModel.js
+++ Models/PostModel.js
@@ -9,14 +9,14 @@ const PostSchema = mongoose.Schema({
     type:String,
     required:true
   },
-  likes:{
+  likes:[{
     type:mongoose.Schema.Types.ObjectId,
     ref:"Like"
-  },
-  comment:{
+  }],
+  comments:[{
     type:mongoose.Schema.Types.ObjectId,
     ref:"Comment"
-  }
+  }]
 });
 
 module.exports = mongoose.model("Post", PostSchema);
