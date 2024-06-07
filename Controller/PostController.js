13e39e7475b135ec7db5461b74c9adb554323391
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-06-07 14:26:01 +0530

    new commit

---
+++ Controller/PostController.js
@@ -0,0 +1,5 @@
+const PostModel=require('../Models/PostModel')
+
+exports.PostController=async(req,res)=>{
+    res.send('This is Ypour Dummy Page')
+}
--- Routes/blogroute.js
+++ Routes/blogroute.js
@@ -1,19 +1,17 @@
 const express=require('express');
 
+
 const router=express.Router();
 
  //Import Controller
 
-
+ const { PostController } = require('../Controller/PostController');
 
 
 
  //Mapping of controllerto the route
 
-
-
-
-
+router.get("/dummy",PostController)
 
  // Export the router
  module.exports=router;
