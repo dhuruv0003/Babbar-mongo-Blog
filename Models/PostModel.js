eefd7e63a8e8e984800d9ff590fab2e6f8337743
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-06-07 14:14:13 +0530

    new commit

--- Models/PostModel.js
+++ Models/PostModel.js
@@ -1,7 +1,18 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 
-const PostSchema=mongoose.Schema({
+const PostSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required:true
+  },
+  body:{
+    type:String,
+    required:true
+  },
+  likes:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Like"
+  }
+});
 
-})
-
-module.exports=mongoose.model('Post',PostSchema)
+module.exports = mongoose.model("Post", PostSchema);
